ea4b432946b53f59c3260273016e7e2336ba6309
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-04-15 21:24:33 +0900

    update count without redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hi')
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+number.innerText = count;
+
+const updateCount = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count = count + 1;
+  updateCount();
+};
+
+const handleMinus = () => {
+  count = count - 1;
+  updateCount();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
