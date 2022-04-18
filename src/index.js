8c32a6fb3443ea1c72a8fa78f945e05e6a30c697
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-04-18 20:50:59 +0900

    update action type to variable

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS"
+
 const countModifier = (count = 0, action) => {
   // reducer
   switch (action.type) {
-    case "ADD":
+    case ADD:
       return (count = count + 1);
-    case "MINUS":
+    case MINUS:
       return (count = count - 1);
     default:
-      return;
+      return count;
   }
 };
 
@@ -23,8 +26,8 @@ const countStore = createStore(countModifier);
 const onChange = () => {number.innerText = countStore.getState()}
 countStore.subscribe(onChange)
 
-const handleAdd = () => {countStore.dispatch({type: "ADD"})}
-const handleMinus = () => {countStore.dispatch({type: "MINUS"})}
+const handleAdd = () => {countStore.dispatch({type: ADD})}
+const handleMinus = () => {countStore.dispatch({type: MINUS})}
 
 add.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
