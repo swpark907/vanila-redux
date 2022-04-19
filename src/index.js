aba12f7197da6389da89479e616dc97bd348fdea
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-04-19 17:32:37 +0900

    update delete function

--- src/index.js
+++ src/index.js
@@ -10,57 +10,60 @@ const DELETE_TODO = "DELETE_TODO";
 const initialState = {
   text: [],
   name: [],
+};
+
+const addTodo = (todo) => {
+  return { type: ADD_TODO, todo };
+};
+
+const deleteTodo = (id) => {
+  return { type: DELETE_TODO, id };
 }
 
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, {text: action.todo, id: Date.now()}];
+      return [...state, { text: action.todo, id: Date.now() }];
     case DELETE_TODO:
-      return [];
+      return state.filter(list => action.id !== list.id);
     default:
       return state;
   }
 };
 
+const store = createStore(reducer);
 
+const dispatchDeleteTodo = (e) => {
+  const $target = e.target.closest("li");
+  const id = Number($target.id);
+  store.dispatch(deleteTodo(id));
+};
+
+const dispatchAddTodo = (todo) => {
+  store.dispatch(addTodo(todo));
+};
 
 const renderList = () => {
-  lists.innerHTML = ''
+  lists.innerHTML = "";
   const state = store.getState();
   state.map((list) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.id = list.id;
     li.innerText = list.text;
-    const btn = document.createElement('button');
-    btn.innerText = 'DEL';
-    btn.addEventListener('click', deleteTodo)
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteTodo);
     lists.appendChild(li);
     li.appendChild(btn);
-  })
-  
-  // lists.innerHTML = state.map((list) => (
-  //   `
-  //     <li id='${list.id}'>
-  //       ${list.text}
-  //       <button>DEL</button>
-  //     </li>
-  //   `
-  // )).join('')
-}
+  });
+};
 
-const addTodo = todo => {
-  store.dispatch({ type: ADD_TODO, todo: todo });
-}
-
-
-const store = createStore(reducer);
-store.subscribe(renderList);
 const onSubmit = (e) => {
   e.preventDefault();
   const todo = input.value;
-  addTodo(todo);
+  dispatchAddTodo(todo);
   input.value = "";
 };
 
+store.subscribe(renderList);
 form.addEventListener("submit", onSubmit);
