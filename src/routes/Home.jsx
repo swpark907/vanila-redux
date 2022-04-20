b04c5dc5ae08091a4f99feb0073b10c74e63bc21
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-04-20 23:54:02 +0900

    update dispatch

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,28 +1,42 @@
 import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-const Home = () => {
-
+const Home = ({toDos, addToDo}) => {
   const [text, setText] = useState("");
   const onChange = (e) => {
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    setText("")
-  }
+    addToDo(text);
+    setText("");
+  };
 
-  useEffect(() => {console.log(text)}, [text])
+  useEffect(() => {
+    console.log(text);
+  }, [text]);
 
   return (
     <>
       <h2>To Do</h2>
       <form onSubmit={onSubmit}>
-        <input type="text" value={text} onChange={onChange}/>
+        <input type="text" value={text} onChange={onChange} />
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 };
 
-export default Home;
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+    addToDo: text => dispatch(actionCreators.addToDo(text))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
